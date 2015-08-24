544b7c0b4640610064ade8a1b519b6ad58a6e003
NorZZa <jordanb-t@hotmail.com>
2015-08-24 20:33:06 +1000

    Adding initial files
    Added all files as of Week1(6) on Assessment 2

---
+++ Vector2.js
@@ -0,0 +1,17 @@
+var Vector2 = function()
+{
+	this.x = 0;
+	this.y = 0;
+};
+
+Vector2.prototype.set = function(x,y)
+{
+	this.x = y;
+	this.y = y;
+}
+
+//todos
+// Normalize() im fairly certain this will = nothing 
+//add
+//subtract
+//multiplyScalar(num)
